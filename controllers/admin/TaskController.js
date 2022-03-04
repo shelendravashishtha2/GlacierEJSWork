5fa0cb18ff0163c98ae3a694bce30591d3f37c29
Alpesh Trivedi <99736497+alpeshgls@users.noreply.github.com>
2022-03-04 21:15:32 +0530

    Validation

--- controllers/admin/TaskController.js
+++ controllers/admin/TaskController.js
@@ -13,6 +13,9 @@ const Joi = require("joi");
 const CategoryFrcMaster = require("../../models/CategoryFrcMaster");
 const CategoryFrcAssign = require("../../models/CategoryFrcAssign");
 const { capitalizeFirstLetter } = require("../../helper/commonHelpers");
+const niv = require('node-input-validator');
+const { assert } = require("console");
+const { Validator } = require('node-input-validator');
 
 // Create Task Page
 exports.categoryAssignment = async (req, res) => {
@@ -97,42 +100,33 @@ exports.assignCategorySubmit = async (req, res) => {
 		if (validation.error) {
 			return res.send(response.error(400, validation.error.details[0].message, []));
 		}
-		let {propertyId, categoryId, operationTeamId} = req.body;
+		let { propertyId, categoryId, operationTeamId } = req.body;
 		for (let i = 0; i < req.body.categoryId.length; i++) {
-			let alreadyExists = await CategoryAssign.exists({ propertyId: req.body.propertyId, categoryId: req.body.categoryId[i] });
-			if (!alreadyExists) {
-				let created = await CategoryAssign.create({
-					propertyId: req.body.propertyId,
-					categoryId: req.body.categoryId[i],
-					operationTeamId: req.body.operationTeamId,
+			let created = await CategoryAssign.create({
+				propertyId: req.body.propertyId,
+				categoryId: req.body.categoryId[i],
+				operationTeamId: req.body.operationTeamId,
+			})
+			let categoryFrc = await CategoryFrcMaster.find({ categoryId: req.body.categoryId[i] });
+			let frcAssigns = []
+			categoryFrc.map((item) => {
+				frcAssigns.push({
+					propertyId: propertyId,
+					assignCategoryId: created._id,
+					checklist_id: item.checklist_id,
+					checklist_name: item.checklist_name,
+					type: item.type,
+					form: item.form,
+					frequency: item.frequency,
+					month: item.month,
+					date: item.date,
+					day: capitalizeFirstLetter(item.day)
 				})
-				let categoryFrc = await CategoryFrcMaster.find({categoryId: req.body.categoryId[i]});
-				let frcAssigns = []
-				categoryFrc.map((item)=>{
-					frcAssigns.push({
-						propertyId: propertyId,
-						assignCategoryId: created._id,
-						checklist_id: item.checklist_id,
-						checklist_name: item.checklist_name,
-						type: item.type,
-						form: item.form,
-						frequency: item.frequency,
-						month: item.month,
-						date: item.date,
-						day: capitalizeFirstLetter(item.day)
-					})
-				})
-				await CategoryFrcAssign.insertMany(frcAssigns);
-
-			} else {
-				await CategoryAssign.updateOne({ propertyId: req.body.propertyId, categoryId: req.body.categoryId[i] }, {
-					operationTeamId: req.body.operationTeamId,
-					deleted: 0
-				})
-			}
+			})
+			await CategoryFrcAssign.insertMany(frcAssigns);
 		}
 		// return res.redirect(req.baseUrl+'/category-assignment?message=Task Assign Successfully');
-		return res.redirect(req.baseUrl+'/category-assignment');
+		return res.redirect(req.baseUrl + '/category-assignment');
 	} catch (error) {
 		errorLog(__filename, req.originalUrl, error);
 		errorMessage = "Something want wrong";
@@ -148,8 +142,8 @@ exports.assignCategory = async (req, res) => {
 
 		let taskData = await CategoryAssign.distinct("propertyId", {});
 		let query = {};
-		if(taskData.length > 0){
-			query = {_id: {$nin: taskData}}
+		if (taskData.length > 0) {
+			query = { _id: { $nin: taskData } }
 		}
 		let propertyData = await Property.find(query, { property_name: 1 });
 		let allCategory = await CategoryMaster.find({ "status": 1 }, { category_name: 1 });
@@ -168,19 +162,53 @@ exports.updateAssignCategory = async (req, res) => {
 	try {
 		res.locals.title = 'Task List';
 		res.locals.session = req.session;
-
-		let schema = Joi.object({
-			propertyId: Joi.required(),
-			categoryId: Joi.required(),
-			operationTeamId: Joi.required()
+		const validate = new Validator(req.body, {
+			propertyId: 'required',
 		});
-		let validation = schema.validate(req.body, __joiOptions);
-		if (validation.error) {
-			return res.send(response.error(400, validation.error.details[0].message, []));
+		const matched = await validate.check();
+		let errors = {propertyId:{},operationTeamId:{}} 
+		if (!matched) {
+			errors.propertId = {errMsg: 'Please select property name'}
+		} else if(req.body.categoryId.length > 0 && req.body.operationTeamId===undefined) {
+			errors.operationTeamId = {errMsg: 'Please select operation team(s)'};
 		}
-		let {propertyId, categoryId, operationTeamId} = req.body;
-		await CategoryAssign.updateMany({propertyId: req.body.propertyId},{deleted:0});
-		await CategoryFrcAssign.updateMany({propertyId: req.body.propertyId},{deleted:1});
+		console.log(errors);
+		console.log(req.body.categoryId.length);
+		console.log(req.body.operationTeamId);
+		console.log(req.body.categoryId);
+		if(Object.keys(errors.propertyId).length > 0 || Object.keys(errors.operationTeamId).length > 0){
+			req.session.errors = errors;
+			return res.redirect('back');
+		}
+		// let schema = Joi.object({
+		// 	propertyId: Joi.required(),
+		// 	categoryId: Joi.optional(),
+		// 	operationTeamId: Joi.when('categoryId',{
+		// 		is: Joi.exist(),
+		// 		then: Joi.required(),
+		// 		otherwise: Joi.optional()
+		// 	})
+		// });
+		// __joiOptions.abortEarly= false;
+		// let validation = schema.validate(req.body, __joiOptions);
+		// if (validation.error) {
+		// 	console.log(validation.error.details[0], 'Validation errors');
+		// 	let errors = {}
+		// 	if(validation.error.details.length > 0){
+		// 		if(validation.error.details[0].path[0] === 'operationTeamId'){
+		// 			errors.errMsg = validation.error.details[0].message;
+		// 			errors.inputData = req.body;
+		// 		}else{
+
+		// 		}
+		// 	}
+		// 	req.session.error = {};
+		// 	return res.redirect('back');
+		// }
+		
+		let { propertyId, categoryId, operationTeamId } = req.body;
+		await CategoryAssign.updateMany({ propertyId: req.body.propertyId }, { deleted: 0 });
+		await CategoryFrcAssign.updateMany({ propertyId: req.body.propertyId }, { deleted: 1 });
 		for (let i = 0; i < req.body.categoryId.length; i++) {
 			let alreadyExists = await CategoryAssign.exists({ propertyId: req.body.propertyId, categoryId: categoryId[i] });
 			if (!alreadyExists) {
@@ -189,9 +217,9 @@ exports.updateAssignCategory = async (req, res) => {
 					categoryId: req.body.categoryId[i],
 					operationTeamId: req.body.operationTeamId,
 				})
-				let categoryFrc = await CategoryFrcMaster.find({categoryId: categoryId[i]});
+				let categoryFrc = await CategoryFrcMaster.find({ categoryId: categoryId[i] });
 				let frcAssigns = []
-				categoryFrc.map((item)=>{
+				categoryFrc.map((item) => {
 					frcAssigns.push({
 						propertyId: propertyId,
 						assignCategoryId: created._id,
@@ -210,39 +238,39 @@ exports.updateAssignCategory = async (req, res) => {
 				let assignCategory = await CategoryAssign.findOneAndUpdate({ propertyId: propertyId, categoryId: categoryId[i] }, {
 					operationTeamId: req.body.operationTeamId,
 					deleted: 1
-				},{ new: true,runValidators: true });
+				}, { new: true, runValidators: true });
 
-				let categoryFrc = await CategoryFrcMaster.find({categoryId: categoryId[i]});
+				let categoryFrc = await CategoryFrcMaster.find({ categoryId: categoryId[i] });
 				let frcAssigns = [];
-					for(let j=0; j < categoryFrc.length; j++ ){
-						let item = categoryFrc[j];
-						let categoryFrcAssignExists = await CategoryFrcAssign.exists({ propertyId: propertyId, assignCategoryId: assignCategory._id, checklist_id: item.checklist_id });
-						if(!categoryFrcAssignExists){
-							frcAssigns.push({
-								propertyId: propertyId,
-								assignCategoryId: assignCategory._id,
-								checklist_id: item.checklist_id,
-								checklist_name: item.checklist_name,
-								type: item.type,
-								form: item.form,
-								frequency: capitalizeFirstLetter(item.frequency),
-								month: item.month,
-								date: item.date,
-								day: capitalizeFirstLetter(item.day)
-							})
-						}else{
-							await CategoryFrcAssign.updateOne({ propertyId: propertyId, checklist_id: item.checklist_id, assignCategoryId: assignCategory._id },{deleted: 0});
-						}
+				for (let j = 0; j < categoryFrc.length; j++) {
+					let item = categoryFrc[j];
+					let categoryFrcAssignExists = await CategoryFrcAssign.exists({ propertyId: propertyId, assignCategoryId: assignCategory._id, checklist_id: item.checklist_id });
+					if (!categoryFrcAssignExists) {
+						frcAssigns.push({
+							propertyId: propertyId,
+							assignCategoryId: assignCategory._id,
+							checklist_id: item.checklist_id,
+							checklist_name: item.checklist_name,
+							type: item.type,
+							form: item.form,
+							frequency: capitalizeFirstLetter(item.frequency),
+							month: item.month,
+							date: item.date,
+							day: capitalizeFirstLetter(item.day)
+						})
+					} else {
+						await CategoryFrcAssign.updateOne({ propertyId: propertyId, checklist_id: item.checklist_id, assignCategoryId: assignCategory._id }, { deleted: 0 });
+					}
 				}
-				if(frcAssigns.length > 0){
+				if (frcAssigns.length > 0) {
 					await CategoryFrcAssign.insertMany(frcAssigns);
 				}
-				
+
 			}
 
 		}
 		req.flash('success_msg', 'Task Assign Successfully');
-		return res.redirect(req.baseUrl+'/category-assignment');
+		return res.redirect(req.baseUrl + '/category-assignment');
 
 	} catch (error) {
 		errorLog(__filename, req.originalUrl, error);
@@ -258,12 +286,12 @@ exports.editAssignCategory = async (req, res) => {
 
 		let taskData = await CategoryAssign.findOne({ propertyId: req.params.id });
 		if (!taskData) {
-			return res.redirect(req.baseUrl+'/category-assignment');
+			return res.redirect(req.baseUrl + '/category-assignment');
 		}
 
 		let propertyData = await Property.findOne({ _id: req.params.id }, { property_name: 1 });
 		if (!propertyData) {
-			return res.redirect(req.baseUrl+'/category-assignment');
+			return res.redirect(req.baseUrl + '/category-assignment');
 		}
 
 		let allCategory = await CategoryMaster.find({ "status": 1 }, { category_name: 1 }).lean();
@@ -274,7 +302,7 @@ exports.editAssignCategory = async (req, res) => {
 			let status = false;
 			for (let j = 0; j < assignCategory.length; j++) {
 				const assignCategoryData = assignCategory[j];
-				if (String(allCategoryData._id) == String(assignCategoryData.categoryId) && assignCategoryData.status==1) {
+				if (String(allCategoryData._id) == String(assignCategoryData.categoryId) && assignCategoryData.status == 1) {
 					status = true;
 				}
 			}
@@ -320,16 +348,16 @@ exports.viewPropertyAssignCategory = async (req, res) => {
 
 		let property = await Property.findOne({ _id: req.params.id }, { property_name: 1 });
 		let categoryIds = await CategoryAssign.find({ propertyId: req.params.id }).distinct("categoryId");
-		let categoryData = await CategoryMaster.find({_id: {$in: categoryIds}});
+		let categoryData = await CategoryMaster.find({ _id: { $in: categoryIds } });
 
-		let operationTeamIds = await CategoryAssign.find({propertyId: req.params.id}).distinct("operationTeamId");
-		let operationTeams = await User.find({_id: {$in: operationTeamIds}});
+		let operationTeamIds = await CategoryAssign.find({ propertyId: req.params.id }).distinct("operationTeamId");
+		let operationTeams = await User.find({ _id: { $in: operationTeamIds } });
 
-		let managerIds = await CategoryAssign.find({propertyId: req.params.id}).distinct("managerId");
-		let managers = await User.find({_id: {$in: managerIds}});
+		let managerIds = await CategoryAssign.find({ propertyId: req.params.id }).distinct("managerId");
+		let managers = await User.find({ _id: { $in: managerIds } });
 
-		let supervisorIds = await CategoryAssign.find({propertyId: req.params.id}).distinct("supervisorId");
-		let supervisors = await User.find({_id: {$in: supervisorIds}});
+		let supervisorIds = await CategoryAssign.find({ propertyId: req.params.id }).distinct("supervisorId");
+		let supervisors = await User.find({ _id: { $in: supervisorIds } });
 
 		// let operationTeams = [];
 		// let supervisors = [];
@@ -358,12 +386,12 @@ exports.viewPropertyAssignCategory = async (req, res) => {
 		// }
 
 		return res.render('Admin/Task/view', {
-            property_name: property.property_name,
-            categoryData: categoryData,
-            managers: managers,
-            supervisors: supervisors,
-            operationTeams: operationTeams,
-        })
+			property_name: property.property_name,
+			categoryData: categoryData,
+			managers: managers,
+			supervisors: supervisors,
+			operationTeams: operationTeams,
+		})
 
 	} catch (error) {
 		errorLog(__filename, req.originalUrl, error);
